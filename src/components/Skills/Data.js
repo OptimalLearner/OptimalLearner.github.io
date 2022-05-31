c23f4efdfbef393ebb90cd54cbbcc7aa414d2c63
Keval Dhanani <dhanani.kev01@gmail.com>
2022-05-31 15:49:23 +0530

    Updated Skills Component by Moving Data in Different File

---
+++ src/components/Skills/Data.js
@@ -0,0 +1,96 @@
+import { FaHtml5, FaBootstrap, FaReact, FaNodeJs, FaPython, FaJava, FaFigma, FaGitSquare, FaGithub, FaNpm } from 'react-icons/fa';
+import { DiCss3, DiMysql } from 'react-icons/di';
+import { IoLogoJavascript } from 'react-icons/io';
+import { SiJquery, SiFlask, SiExpress, SiMongodb, SiCplusplus, SiInkscape, SiSublimetext, SiVisualstudiocode } from 'react-icons/si';
+import { BiDevices } from 'react-icons/bi';
+
+export const skills = [
+    {
+        skills_name: 'Python',
+        skills_logo: <FaPython />
+    },
+    {
+        skills_name: 'C++',
+        skills_logo: <SiCplusplus />
+    },
+    {
+        skills_name: 'Java',
+        skills_logo: <FaJava />
+    },
+    {
+        skills_name: 'HTML',
+        skills_logo: <FaHtml5 />
+    },
+    {
+        skills_name: 'CSS',
+        skills_logo: <DiCss3 />
+    },
+    {
+        skills_name: 'JavaScript',
+        skills_logo: <IoLogoJavascript />
+    },
+    {
+        skills_name: 'Bootstrap',
+        skills_logo: <FaBootstrap />
+    },
+    {
+        skills_name: 'jQuery',
+        skills_logo: <SiJquery />
+    },
+    {
+        skills_name: 'ReactJS',
+        skills_logo: <FaReact />
+    },
+    {
+        skills_name: 'Responsive Design',
+        skills_logo: <BiDevices />
+    },
+    {
+        skills_name: 'Flask',
+        skills_logo: <SiFlask />
+    },
+    {
+        skills_name: 'NodeJS',
+        skills_logo: <FaNodeJs />
+    },
+    {
+        skills_name: 'ExpressJS',
+        skills_logo: <SiExpress />
+    },
+    {
+        skills_name: 'MongoDB',
+        skills_logo: <SiMongodb />
+    },
+    {
+        skills_name: 'MySQL',
+        skills_logo: <DiMysql />
+    },
+    {
+        skills_name: 'Git',
+        skills_logo: <FaGitSquare />
+    },
+    {
+        skills_name: 'GitHub',
+        skills_logo: <FaGithub />
+    },
+    {
+        skills_name: 'NPM',
+        skills_logo: <FaNpm />
+    },
+    {
+        skills_name: 'VSCode',
+        skills_logo: <SiVisualstudiocode />
+    },
+    {
+        skills_name: 'Sublime Text',
+        skills_logo: <SiSublimetext />
+    },
+    {
+        skills_name: 'Figma',
+        skills_logo: <FaFigma />
+    },
+    {
+        skills_name: 'InkScape',
+        skills_logo: <SiInkscape />
+    }
+]
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { SkillsContainer, Heading, SkillsType, SkillsList, SkillsBox, SkillsLogo, SkillsName } from './Skills.elements';
-import { FaHtml5, FaBootstrap, FaReact, FaNodeJs, FaPython, FaJava, FaFigma, FaGitSquare, FaGithub, FaNpm } from 'react-icons/fa';
-import { DiCss3, DiMysql } from 'react-icons/di';
-import { IoLogoJavascript } from 'react-icons/io';
-import { SiJquery, SiFlask, SiExpress, SiMongodb, SiCplusplus, SiInkscape, SiSublimetext, SiVisualstudiocode } from 'react-icons/si';
-import { BiDevices } from 'react-icons/bi';
+import { skills } from './Data';
 
 const Skills = () => {
     return (
@@ -13,94 +9,12 @@ const Skills = () => {
                 <Heading data-shadow='Skills'> Skills </Heading>
                 <SkillsType>
                     <SkillsList>
-                        <SkillsBox>
-                            <SkillsLogo> <FaPython /> </SkillsLogo>
-                            <SkillsName> Python </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiCplusplus /> </SkillsLogo>
-                            <SkillsName> C++ </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaJava /> </SkillsLogo>
-                            <SkillsName> Java </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaHtml5 /> </SkillsLogo>
-                            <SkillsName> HTML </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <DiCss3 /> </SkillsLogo>
-                            <SkillsName> CSS </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <IoLogoJavascript /> </SkillsLogo>
-                            <SkillsName> JavaScript </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaBootstrap /> </SkillsLogo>
-                            <SkillsName> Bootstrap </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiJquery /> </SkillsLogo>
-                            <SkillsName> jQuery </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaReact /> </SkillsLogo>
-                            <SkillsName> ReactJS </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <BiDevices /> </SkillsLogo>
-                            <SkillsName> Responsive Design </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiFlask /> </SkillsLogo>
-                            <SkillsName> Flask </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaNodeJs /> </SkillsLogo>
-                            <SkillsName> NodeJS </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiExpress /> </SkillsLogo>
-                            <SkillsName> ExpressJS </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiMongodb /> </SkillsLogo>
-                            <SkillsName> MongoDB </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <DiMysql /> </SkillsLogo>
-                            <SkillsName> MySQL </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaGitSquare /> </SkillsLogo>
-                            <SkillsName> Git </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaGithub /> </SkillsLogo>
-                            <SkillsName> GitHub </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaNpm /> </SkillsLogo>
-                            <SkillsName> NPM </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiVisualstudiocode /> </SkillsLogo>
-                            <SkillsName> VSCode </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiSublimetext /> </SkillsLogo>
-                            <SkillsName> Sublime Text </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <FaFigma /> </SkillsLogo>
-                            <SkillsName> Figma </SkillsName>
-                        </SkillsBox>
-                        <SkillsBox>
-                            <SkillsLogo> <SiInkscape /> </SkillsLogo>
-                            <SkillsName> InkScape </SkillsName>
-                        </SkillsBox>
+                        { skills.map((e) => (
+                            <SkillsBox>
+                                <SkillsLogo> {e.skills_logo} </SkillsLogo>
+                                <SkillsName> {e.skills_name} </SkillsName>
+                            </SkillsBox>
+                        )) }
                     </SkillsList>
                 </SkillsType>
             </SkillsContainer>
