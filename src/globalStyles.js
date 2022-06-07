bee9b0ba0ce9f096d989f31345c5e6a3794d2327
Keval Dhanani <dhanani.kev01@gmail.com>
2022-06-07 22:57:32 +0530

    Scrollbar Design Updated

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -9,6 +9,17 @@ const GlobalStyle = createGlobalStyle`
     body {
         overflow-x: hidden;
     }
+    ::-webkit-scrollbar {
+        width: 15px;
+    }
+    ::-webkit-scrollbar-track {
+        background-color: #232323;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: #FFD93D;
+        border-radius: 0.25rem;
+        border: 2px solid #232323;
+    }
 `;
 
 export default GlobalStyle;
