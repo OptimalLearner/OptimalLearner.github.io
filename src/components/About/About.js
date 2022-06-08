1459760fd905a16d4aa837986ae32be4378236b6
Keval Dhanani <dhanani.kev01@gmail.com>
2022-06-08 16:46:31 +0530

    About Me Info Updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,9 +9,9 @@ const About = () => {
                 <div style={{ position: 'relative' }}>
                     <Heading data-shadow='About Me'> About Me </Heading>
                     <AboutInfo> 
-                        Hi, I'm Keval Dhanani currently in Final Year of Computer Engineering. I'm very passionate about Software Engineering and Blockchain Technology and like to build some creative stuff over the web.
+                        Hi, I'm Keval Dhanani currently in Final Year in Computer Engineering. I'm very passionate about Software Engineering and Blockchain Technology and like to build some creative stuff over the web. I love learning better ways to create seamless user experiences with efficient and scalable code.
                         <br /><br />
-                        Being a hardworking and result-oriented individual, I can handle multiple projects at once and complete within the deadlines without affecting the integrity of my projects. I'm always looking for opportunities to learn and explore new things.    
+                        Being a hardworking and result-oriented individual, I can handle multiple projects at once and complete within the deadlines without affecting the integrity of my projects. I'm always looking for opportunities to work with those who are willing to share their knowledge.  
                     </AboutInfo>
                 </div>
                 <AboutVectorContainer>
